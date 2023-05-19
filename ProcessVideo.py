2ecea286108835dedb06b8f63bb643fdf64990d0
Vitor Cesar Lulio <vitorcesardtb1@hotmail.com>
2023-05-18 22:34:32 -0300

    Melhorias de erros e comentarios

--- ProcessVideo.py
+++ ProcessVideo.py
@@ -3,102 +3,112 @@ import os
 
 def main():
     """
-    1. Reduz frames
-    2. Redimensiona frames
-    3. Joga para escala de cinza
-    4. Delimita area especifica (corte no frame)
+        Ponto de entrada do arquivo, garante execução principal do programa
 
-    return um array com frames otimizados
+        Args:
+            empty
+
+        Returns:
+            array: array com frames.
     """
     global video_path
     video_path = 'video_plate_recognition.MOV'
 
-    decreaseFramesArray()
+    try:
+        decreaseFramesArray()
+    except Exception as e:
+        print(e)
+    # finally:
+        # Código a ser executado sempre
 
 def decreaseFramesArray():
     """
-        Retorna um array com frames
+        Faz uma série de tratamentos no vídeo para melhorar a performance:
+        1. Reduz frames
+        2. Redimensiona frames
+        3. Converte para escala de cinza
+        4. Delimita área específica (corte no frame)
+
+        Args:
+            empty
+
+        Returns:
+            array: array com frames processados.
     """
-    # Abre o vídeo
-    cap = cv2.VideoCapture(video_path, cv2.CAP_FFMPEG)
-    # O CAP_FFMPEG pode ser mais rápido para alguns formatos de vídeo. 
-    # Acelerar a leitura do vídeo com GPU
-    #cv2.CAP_DSHOW ESSE N FUNCIONOU VER COMO USA ELE
+    try:
+        # Abre o vídeo
+        cap = cv2.VideoCapture(video_path, cv2.CAP_FFMPEG)
 
-    # Verifica se o vídeo foi aberto com sucesso
-    if not cap.isOpened():
-        print("Não foi possível abrir o vídeo.")
+        # Verifica se o vídeo foi aberto com sucesso
+        if not cap.isOpened():
+            raise Exception("Não foi possível abrir o vídeo")
 
-    # Fator de redução da taxa de quadros (exemplo: reduzindo pela metade)
-    fator_reducao = 50
+        # Fator de redução da taxa de quadros (exemplo: reduzindo pela metade)
+        fator_reducao = 50
 
-    # Lista para armazenar os frames processados
-    frames_processados = []
+        # Lista para armazenar os frames processados
+        frames_processados = []
 
-    # reduz tamanho do video
-    # define o tamanho desejado
-    # PODE SER QUE DE PRA DIMINUIR MAIS AINDA
-    largura = 640
-    altura = 480
+        # Define o tamanho desejado para redução do video
+        largura = 640
+        altura = 480
 
-    # Lê e processa os frames reduzidos
-    count = 0
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
-        # Processa o frame atual apenas a cada fator de redução
-        if count % fator_reducao == 0:
-            # Faça o processamento desejado no frame aqui
-            # Por exemplo, você pode aplicar filtros, realizar detecção de objetos, etc.
-            
-            # redimensiona o frame, LxA RIMEIRO FAZER ISSO, MAIS INTELIGENTE
-            frame = cv2.resize(frame, (largura, altura))
+        # Lê e processa os frames reduzidos
+        count = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
 
-            # converte para escala de cinza
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            # focar somente na parte que quero do video
-            #area = frame[500:, 300:800]
-            # focar somente na parte que quero do video
-            #area = frame_redimensionado[altura//2:, largura//2:largura]
-
-            #comprimir o video
-           
-            # tira informações inuteis do video
-            #corrigi problemas de iluminação
-
-            ######### DEBUG - START ###########
-            """
-            # Converte o quadro para o formato JPEG
-            success, encoded_frame = cv2.imencode(".jpg", frame)
-            content = encoded_frame.tobytes()
-
-            # Salve o quadro como uma imagem na pasta
-            frame_path = os.path.join('frames_video/', f'frame{count}.jpg')
-            cv2.imwrite(frame_path, frame)"""
-            # acompanhar frames
-            cv2.imshow('Frames', frame)
-            
-            ######### DEBUG - END ###########
-
-            # Armazena o frame processado na lista
-            frames_processados.append(frame)
-
-            # Espera por uma tecla pressionada para avançar para o próximo frame
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if not ret:
                 break
-        
-        count += 1
+            
+            # Processa o frame atual apenas a cada fator de redução
+            if count % fator_reducao == 0:
+                # Redimensiona o frame
+                frame = cv2.resize(frame, (largura, altura))
 
-    # Libera os recursos
-    cap.release()
+                # Converte para escala de cinza
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # descometnar se usar cv2.imshow
-    cv2.destroyAllWindows()
+                # focar somente na parte que quero do video
+                #area = frame[500:, 300:800]
+                # focar somente na parte que quero do video
+                #area = frame_redimensionado[altura//2:, largura//2:largura]
 
-    return frames_processados
+                # Aplica a equalização de histograma para melhorar o contraste
+                # frame = cv2.equalizeHist(frame)
+
+                ######### DEBUG - START ###########
+                """
+                # Converte o quadro para o formato JPEG
+                success, encoded_frame = cv2.imencode(".jpg", frame)
+                content = encoded_frame.tobytes()
+
+                # Salve o quadro como uma imagem na pasta
+                frame_path = os.path.join('frames_video/', f'frame{count}.jpg')
+                cv2.imwrite(frame_path, frame)"""
+                # acompanhar frames
+                cv2.imshow('Frames', frame)
+                ######### DEBUG - END ###########
+
+                # Armazena o frame processado na lista
+                frames_processados.append(frame)
+
+                # Espera por uma tecla pressionada para avançar para o próximo frame
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            
+            count += 1
+
+        # Libera os recursos
+        cap.release()
+
+        # descometnar se usar cv2.imshow
+        cv2.destroyAllWindows()
+
+        return frames_processados
+
+    except Exception as e:
+        print("Ocorreu um erro:", str(e))
 
 if __name__ == "__main__":
     main()
