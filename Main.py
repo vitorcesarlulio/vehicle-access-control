2cf46be4fd96dde1b59501ba281a6aa418647cd0
Vitor Cesar Lulio <vitorcesardtb1@hotmail.com>
2023-05-18 21:12:37 -0300

    Arquitetado Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,17 @@
 import PlateRecognition
 import AccessControl
+import ProcessVideo
 
 
 def main():
-    plate = PlateRecognition.main()
-    print(plate)
+    # Processo video, otimiza
+    processVideo = ProcessVideo.main()
+
+    # Reconhece objetos e extrai texto da placa
+    # plateRecognition = PlateRecognition.main()
+
+    # Valida a placa
+    # accessControl = AccessControl.main()
 
 
 if __name__ == "__main__":
